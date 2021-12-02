0d12c3fb0cd21b2a3d645d611f08154540f2fbca
Colin Jeanne <colinjeanne@hotmail.com>
2021-12-02 08:19:14 +0000

    Add type checking to encoding.js
    This change adds the first set of type checking. Since TypeScript can run over JavaScript files no build system is needed.

--- src/helpers/encoding.js
+++ src/helpers/encoding.js
@@ -1,11 +1,24 @@
-export function encodeRle(arr, maxRunLength) {
-  let count = 0;
-  let previousValue = undefined;
-  return arr.reduce((encoded, value, index) => {
-    if (index === 0) {
-      previousValue = value;
-    }
+/**
+ * @typedef {(0 | 1)[]} BitArray
+ */
 
+/**
+ * Performs a run-length encoding on the given array
+ *
+ * @template T
+ * @param {T[]} arr
+ * @param {number} maxRunLength
+ */
+export function encodeRle(arr, maxRunLength) {
+  if (arr.length === 0) {
+    return [];
+  }
+
+  let count = 0;
+
+  let previousValue = arr[0];
+  return arr.reduce((/** @type [number, T][] */ encoded, value, index) => {
+    /** @type {[number, T][]} */
     let addition = [];
 
     if (value !== previousValue || count === maxRunLength) {
@@ -24,8 +37,14 @@ export function encodeRle(arr, maxRunLength) {
   }, []);
 }
 
+/**
+ * Decodes a run-length encoded array into the full array
+ *
+ * @template T
+ * @param {[number, T][]} rleEncoded
+ */
 export function decodeRle(rleEncoded) {
-  return rleEncoded.reduce((arr, rle) => [
+  return rleEncoded.reduce((/** @type T[] */ arr, rle) => [
     ...arr,
     ...Array(rle[0]).fill(rle[1])
   ], []);
@@ -33,23 +52,65 @@ export function decodeRle(rleEncoded) {
 
 const base64Symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
+/**
+ * Gets the bit at the given index
+ *
+ * @param {number} num
+ * @param {number} bitIndex
+ */
+function getBit(num, bitIndex) {
+  if (num >> bitIndex) {
+    return 1;
+  }
+
+  return 0;
+}
+
+/**
+ * Sets the bit at the given index to the given bit value
+ *
+ * @param {number} num
+ * @param {number} bitIndex
+ * @param {0 | 1} bit
+ */
+function setBit(num, bitIndex, bit) {
+  return (num & ~(1 << bitIndex)) | (bit << bitIndex);
+}
+
+/**
+ * Encodes a numeric value as an array of bits
+ *
+ * @param {number} value
+ * @param {number} bitCount
+ */
 export function encodeToBits(value, bitCount) {
+  /** @type {BitArray} */
   const bits = [];
   for (let i = 0; i < bitCount; ++i) {
-    bits.push((value >> (bitCount - i - 1)) & 0x1);
+    bits.push(getBit(value, bitCount - i - 1));
   }
 
   return bits;
 }
 
+/**
+ * Encodes an array of no more than 6 bits to a Base-64 symbol
+ *
+ * @param {BitArray} bits
+ */
 export function bitsToBase64Symbol(bits) {
   const value = bits.reduce(
-    (value, bit, index) => value | (bit << (5 - index)),
+    (/** @type {number} */ value, bit, index) => setBit(value, 5 - index, bit),
     0
   );
   return base64Symbols[value];
 }
 
+/**
+ * Encodes an array of bits to Base-64
+ *
+ * @param {BitArray} bits
+ */
 export function base64EncodeBits(bits) {
   let encoded = "";
   for (let currentBit = 0; currentBit < bits.length; currentBit += 6) {
@@ -59,19 +120,31 @@ export function base64EncodeBits(bits) {
   return encoded;
 }
 
+/**
+ * Encodes a Base-64 symbol to an array of bits
+ *
+ * @param {string} symbol
+ * @returns {BitArray}
+ */
 export function base64SymbolToBits(symbol) {
   const index = base64Symbols.indexOf(symbol);
   return [
-    (index >> 5) & 0x1,
-    (index >> 4) & 0x1,
-    (index >> 3) & 0x1,
-    (index >> 2) & 0x1,
-    (index >> 1) & 0x1,
-    index & 0x1,
+    getBit(index, 5),
+    getBit(index, 4),
+    getBit(index, 3),
+    getBit(index, 2),
+    getBit(index, 1),
+    getBit(index, 0),
   ];
 }
 
+/**
+ * Decodes a Base-64 string to an array of bits
+ *
+ * @param {string} encoded
+ */
 export function decodeBase64ToBits(encoded) {
+  /** @type {BitArray} */
   const bits = [];
   for (const chr of encoded) {
     if (!base64Symbols.includes(chr)) {
@@ -84,8 +157,15 @@ export function decodeBase64ToBits(encoded) {
   return bits;
 }
 
+/**
+ * Reads a number of bits from a bit array as a numeric value
+ *
+ * @param {BitArray} bitsArray
+ * @param {number} start
+ * @param {number} count
+ */
 export function readBits(bitsArray, start, count) {
-  return bitsArray.slice(start, start + count).reduce((value, bit, index) => {
-    return value | (bit << (count - index - 1));
+  return bitsArray.slice(start, start + count).reduce((/** @type {number} */ value, bit, index) => {
+    return setBit(value, count - index - 1, bit);
   }, 0);
 }
